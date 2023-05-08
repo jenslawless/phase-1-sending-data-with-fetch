dcfdc3d0b38a42d2471db348686e307b1794a47d
Jensen Lawless <jensenlawless@gmail.com>
2023-05-07 20:42:47 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-// Add your code here
+// const userData = {
+//     name: "Steve",
+//     email: "steve@steve.com",
+// };
+
+// const confObj = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         name,
+//         email
+//     })
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            document.body.innerHTML = object["id"]
+        })
+
+        .catch(function (error) {
+            document.body.innerHTML = error.message
+        })
+
+}
+
+
+
+// function submitData(name, email) {
+//     return fetch('http://localhost:3000/users', {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name,
+//             email
+//         })
+//     })
+//         .then(function (response) {
+//             return response.json()
+//         })
+//         .then(function (object) {
+//             document.body.innerHTML = object["id"]
+//         })
+//         .catch(function (error) {
+//             document.body.innerHTML = error.message
+//         })
+// }
